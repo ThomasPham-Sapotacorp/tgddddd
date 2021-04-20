d2be887fc989d323884f4ac74c39dce6c152efca
tienthanh5406 <62203259+tienthanh5406@users.noreply.github.com>
2021-04-21 00:04:12 +0700

    first commit

---
+++ public/scripts/hihi.js
@@ -0,0 +1,13 @@
+
+function ShowMoreFooterSupportLink() {
+
+    var hiddens = document.getElementsByClassName('hidden');
+    for (let i = 0; i < hiddens.length; i++) {
+        // console.log(hiddens[i].className);
+        // hiddens[i].setAttribute("class", "");
+        hiddens[i].className = "nn";
+    }
+
+}
+
+
